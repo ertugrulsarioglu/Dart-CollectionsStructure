5e116794241d94a531bc7b7eebbda59d74602e35
ertugrul <sariertugrull@gmail.com>
2022-03-20 01:24:45 +0300

    Initial commit

--- lib/ListCalismasi.dart
+++ lib/ListCalismasi.dart
@@ -8,5 +8,27 @@ void main(){
 
   var plakalar = [16,34,6];
 
+  var meyveler = <String>[];
 
+  meyveler.add("Çilek"); //0. index
+  meyveler.add("Muz"); //1. index
+  meyveler.add("Elma"); //2. index
+  meyveler.add("Kivi"); //3. index
+  meyveler.add("Kiraz"); //4. index
+
+  print(meyveler);
+
+  meyveler.add("Mandalina"); // add komutu her zaman en sona ekler
+  print(meyveler);
+
+  meyveler[2] = "Ananas"; //elma yerine ananası ekledik. yani 2.indexi güncelledik
+  print(meyveler);
+
+  meyveler.insert(3, "Portakal"); //3.indexe portakalı ekledi diğerlerini sağa kaydırdı.
+  print(meyveler);
+
+  String str = meyveler[0];
+  print(str);
+
+  print(meyveler[2]);
 }
